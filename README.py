71cc28a69809eb249ad1241d1b771a24e80a4037
Eduar Gutierrez <eduardbv2007@gmail.com>
2025-02-03 12:28:26 -0500

    Create README.py
    esto es alguito

---
+++ README.py
@@ -0,0 +1,4 @@
+## Hola muchacho como estas espero que bien y ahora mira este temita
+Nombre = input("Muchachon cual es tu nombre")
+print(f"Hola", nombre, " ¿Como estas tu el dia de hoy?")
+input("Your day is: ")
