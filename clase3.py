3690aefb2e69848a87e9505951c2166740ccfeb9
jdm18 <jdaniel2427@gmail.com>
2025-02-04 10:21:50 -0500

    Origin Master

--- clase3.py
+++ clase3.py
@@ -2,4 +2,6 @@ num1 = int(input("Dame el numero de telefono: "))
 
 print(num1)
 
+## Esta clase trato de repositorios y github, git es la aplicacion y git hub es la nube osea el "hab" asi que poca cosa realmente, pero esto es bueno para hjacerlo
 
+hola = input("hola que tal: ")
