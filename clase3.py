4cb9bf5fb8554069a940fb8cccc297941d501046
jdm18 <jdaniel2427@gmail.com>
2025-02-04 12:27:03 -0500

    Hola mundo

--- clase3.py
+++ clase3.py
@@ -1,3 +1,4 @@
+"""""
 num1 = int(input("Dame el numero de telefono: "))
 
 print(num1)
@@ -5,3 +6,30 @@ print(num1)
 ## Esta clase trato de repositorios y github, git es la aplicacion y git hub es la nube osea el "hab" asi que poca cosa realmente, pero esto es bueno para hjacerlo
 
 hola = input("hola que tal: ")
+"""
+""" Clase numero 4 pai"""
+edad = 64
+lista_nombre = ["Hola", 53, True, False, 3.14162538]
+print(list(lista_nombre))
+print(len(lista_nombre))
+mondogo = [input("hola dame tu nombre"), "suero", "zanahoria", "arvejas", "ajo", "color", "sabor"]
+print(mondogo)
+print(mondogo[0], lista_nombre[2])
+lista = {input("hellodwa: ")}
+print(lista)
+
+
+
+Mondongiño = ["Arvejas", "Zanahoria", "ajo", "sabor", "Cebolla de rama", "Mondongo", "sal"]
+Mondongiño.extend("2")
+print(Mondongiño)
+Mondongiño.append("Vainilla descremada"[1])
+Mondongiño.extend(edad)
+print(Mondongiño)
+
+copia_mondongiño = Mondongiño.copy()
+print(Mondongiño.count("Mondogo"))
+print(Mondongiño.index("Cebolla de rama"))
+Mondongiño.remove("ajo")
+print(Mondongiño)
+Mondongiño.sort()
