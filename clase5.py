fb0048f881969927e95563d2ecc1cc7ee4b7b3c8
jdm18 <eduardbv2007@gmail.com>
2025-02-05 10:48:28 -0500

    velove

---
+++ clase5.py
@@ -0,0 +1,20 @@
+## Condicionales for while y demas cosas ue ni idea la vrd
+
+Usuario = []
+
+print(Usuario)
+"nombre = input(ingrese el nombre  o x: )"
+"""""
+if(nombre != "x"):
+     Usuario.append(nombre)
+"""""
+
+while True:
+    nombre=input("digite el nombre o x: ")
+
+    if nombre =="x":
+        break
+
+    Usuario.append(nombre)
+
+print("usuarios registrados: ", Usuario)
